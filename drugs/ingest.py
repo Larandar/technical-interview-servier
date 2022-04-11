e1925b748d02d36a8efc7cca2dc03be6ed26266d
Adrien DUDOUIT-EXPOSITO <dudouit.a@sfeir.com>
2022-04-11 11:39:32 +0200

    feat(ingest): use JSON schemas for bq load jobs

--- drugs/ingest.py
+++ drugs/ingest.py
@@ -1,24 +1,62 @@
 """Injest files into bigquery."""
+import json
+import tempfile
 from pathlib import Path
 
 from drugs.operators import bq_load_operator, bq_mk_dataset_operator
+
+TMP_SCHEMA: Path = None
+
+
+def schema_file(schema: dict) -> Path:
+    """Create a schema file from a dictionary."""
+    global TMP_SCHEMA
+    if TMP_SCHEMA is None or not TMP_SCHEMA.exists():
+        TMP_SCHEMA = Path(tempfile.mkstemp(suffix="_schema.json")[1])
+
+    with TMP_SCHEMA.open("w") as f:
+        json.dump(schema, f)
+
+    return TMP_SCHEMA
 
 
 def ingest_drugs(dataset: str):
     """Ingest files into bigquery as raw as possible."""
     bq_mk_dataset_operator(dataset)
+
     bq_load_operator(
         f"{dataset}.drugs_raw_v1",
         Path("inputs/drugs.csv"),
-        "atcode:STRING,drug:STRING",
+        schema_file(
+            [
+                dict(name="atcode", type="STRING", mode="REQUIRED"),
+                dict(name="drug", type="STRING", mode="REQUIRED"),
+            ]
+        ),
     )
     bq_load_operator(
         f"{dataset}.clinical_trials_raw_v1",
         Path("inputs/clinical_trials.csv"),
-        "id:STRING,scientific_title:STRING,date:STRING,journal:STRING",
+        schema_file(
+            [
+                dict(name="id", type="STRING", mode="REQUIRED"),
+                dict(name="scientific_title", type="STRING", mode="REQUIRED"),
+                dict(name="date", type="STRING", mode="REQUIRED"),
+                dict(name="journal", type="STRING", mode="REQUIRED"),
+            ]
+        ),
     )
     bq_load_operator(
         f"{dataset}.pubmed_raw_v1",
         Path("inputs/pubmed.csv"),
-        "id:INTEGER,title:STRING,date:STRING,journal:STRING",
+        schema_file(
+            [
+                dict(name="id", type="INTEGER", mode="REQUIRED"),
+                dict(name="title", type="STRING", mode="REQUIRED"),
+                dict(name="date", type="STRING", mode="REQUIRED"),
+                dict(name="journal", type="STRING", mode="REQUIRED"),
+            ]
+        ),
     )
+
+    TMP_SCHEMA.unlink()
